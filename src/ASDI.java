98c59a1720a77aa3e77afc576af18e235c844247
zVioletta <92064685+zVioletta@users.noreply.github.com>
2023-11-30 15:07:08 -0600

    Cambio completo de código

--- src/ASDI.java
+++ src/ASDI.java
@@ -1,199 +1,153 @@
 import java.util.List;
-import java.util.Stack;
 
 public class ASDI implements Parser {
-    private int i = 0, noTerm = 0, term = 0;
-    private boolean err = false;
-    private Token preAn;
     private final List<Token> tokens;
-    private final Tabla ta = new Tabla();
-    private String prod = "", item = "";
-    private Stack<String> pila = new Stack<>();
+    private Token preAn;
+    private int i = 0;
+    private int err = 0;
 
-    public ASDI(List<Token> tokens) {
+    private void error() {
+        System.out.println("Error on: " + (i + 1) + ". Expecting:" + tokens.get(i));
+        err = 1;
+    }
+
+    public ASDI (List<Token> tokens) {
         this.tokens = tokens;
         preAn = this.tokens.get(i);
     }
 
     @Override
     public boolean parse() {
-        this.pila.push("$");
-        this.pila.push("Q");
-        this.item = pila.pop();
-
-        while (item != null) {
-            if (this.err)
-                return false;
-
-            // ? Comprobar si item es terminal o no terminal.
-            if ((item.equals("Q")) || (item.equals("D")) || (item.equals("P")) || (item.equals("A"))
-                    || (item.equals("A1")) || (item.equals("A2")) || (item.equals("A3")) || (item.equals("T"))
-                    || (item.equals("T1")) || (item.equals("T2")) || (item.equals("T3"))) {
-                System.out.println("»»» First Case");
-                System.out.println("Index\nItem: " + item + ". Pre analysis:  " + preAn + ".");
-                this.buscarIndices(item, this.preAn);
-
-                if (err)
-                    break;
-
-                this.production();
-                System.out.println("Production: " + prod + ".");
-                this.addBatteryInv();
-                this.item = pila.pop();
-                System.out.println("Item: " + this.item + ".");
-                System.out.println("»»» End first case");
-
-            } else if (((item.equals("select")) && (this.preAn.tipo.equals(TipoToken.SELECT)))
-                    || ((item.equals("from")) && (this.preAn.tipo.equals(TipoToken.FROM)))
-                    || ((item.equals("distinct")) && (this.preAn.tipo.equals(TipoToken.DISTINCT)))
-                    || ((item.equals("*")) && (this.preAn.tipo.equals(TipoToken.ASTERISCO)))
-                    || ((item.equals(",")) && (this.preAn.tipo.equals(TipoToken.COMA)))
-                    || ((item.equals(".")) && (this.preAn.tipo.equals(TipoToken.PUNTO)))
-                    || ((item.equals("id")) && (this.preAn.tipo.equals(TipoToken.IDENTIFICADOR)))
-                    || ((item.equals("$")) && (this.preAn.tipo.equals(TipoToken.EOF)))) {
-
-                System.out.println("»»» Second case");
-                // ? Si son iguales
-                item = pila.pop();
-                i++;
-                this.preAn = this.tokens.get(i);
-                System.out.println("Item: " + item + ". Pre analysis:  " + preAn + ".");
-                System.out.println("»»» End second case");
-
-            } else if (item.equals("E")) {
-                System.out.println("»»» Third case");
-                // ? No se inserta nada en la pila
-                item = pila.pop();
-                System.out.println("Item: " + item + ". Pre analysis:  " + preAn + ".");
-                System.out.println("»»» End third case");
-
-            } else if (item.equals("#")) {
-                System.out.println("»»» Errors");
-                this.err = true;
-                System.out.println("La sintaxis es erronea. Token:" + this.preAn.lexema + ". Tipo: " + this.preAn.tipo +
-                        ". position: " + this.preAn.posicion + ".");
-                System.out.println("Item: " + item + ". Pre analysis:  " + preAn + ".");
-                System.out.println("»»» End errors");
+        if (Q()) {
+            if(this.preAn.equals("EOF")) {
+                System.out.println("Success!");
+            } else {
+                error();
             }
-        }
-
-        if (preAn.tipo == TipoToken.EOF && !err && this.pila.isEmpty()) {
-            System.out.println("Consulta correcta");
-            return true;
         } else {
-            System.out.println("Se encontraron errores");
+            error();
+        }
+        return err == 0;
+    }
+
+    private boolean Q() {
+        if (tokens.get(i).equals("select")) {
+            i++;
+            if (D()) {
+                if (tokens.get(i).equals("from")) {
+                    i++;
+                    return T();
+                    } else {
+                        error();
+                    }
+                } else {
+                    error();
+                }
+            }
+        return false;
+    }
+
+    private boolean D() {
+        if (tokens.get(i).equals("distinct")) {
+            i++;
+            return P();
+        } else {
+            return P();
+        }
+    }
+
+    private boolean P() {
+        if (tokens.get(i).equals("*")) {
+            i++;
+        } else {
+            return A();
         }
         return false;
     }
 
-    public void buscarIndices(String noTerm, Token term) {
-        switch (noTerm) {
-            case "Q":
-                this.noTerm = 0;
-                break;
-            case "D":
-                this.noTerm = 1;
-                break;
-            case "P":
-                this.noTerm = 2;
-                break;
-            case "A":
-                this.noTerm = 3;
-                break;
-            case "A1":
-                this.noTerm = 4;
-                break;
-            case "A2":
-                this.noTerm = 5;
-                break;
-            case "A3":
-                this.noTerm = 6;
-                break;
-            case "T":
-                this.noTerm = 7;
-                break;
-            case "T1":
-                this.noTerm = 8;
-                break;
-            case "T2":
-                this.noTerm = 9;
-                break;
-            case "T3":
-                this.noTerm = 10;
-                break;
-            default:
-                err = true;
-                System.out.println("Error. Indice (noTerm) no encontrado");
-                break;
-        }
-
-        switch (term.tipo) {
-            case SELECT:
-                this.term = 0;
-                break;
-            case FROM:
-                this.term = 1;
-                break;
-            case DISTINCT:
-                this.term = 2;
-                break;
-            case ASTERISCO:
-                this.term = 3;
-                break;
-            case COMA:
-                this.term = 4;
-                break;
-            case PUNTO:
-                this.term = 5;
-                break;
-            case IDENTIFICADOR:
-                this.term = 6;
-                break;
-            case EOF:
-                this.term = 7;
-                break;
-            default:
-                err = true;
-                System.out.println("Error. Indice (Terminal) no encontrado");
-                break;
-        }
-    }
-
-    public void production() {
-        this.prod = this.ta.tabla[this.noTerm][this.term];
-    }
-
-    public void addBatteryInv() {
-        System.out.println("»»» Inverse added");
-        char c; // ? Distingue palabras de producciónes
-        String aux = "";
-        int x = this.prod.length() - 1;
-
-        while (x >= 0) {
-            c = this.prod.charAt(x);// ? Asigna último char del lexema
-            if ((c == ' ') || (x == 0)) {
-                if (x == 0)
-                    aux += c;
-
-                System.out.println("Before reverse: " + aux);
-                this.pila.push(this.reverse(aux));
-                aux = "";
-                System.out.println("Battery start: " + pila.peek());
-
+    private boolean A() {
+        if (A2()) {
+            if (A1()) {
+                return true;
             } else {
-                aux += c;
-                System.out.println("X = " + x + " Aux = " + aux);
+                error();
             }
-            x--;
+        }
+        return false;
+    }
+
+    private boolean A1() {
+        if (tokens.get(i).equals(",")) {
+            i++;
+            if (A()) {
+                return true;
+            } else {
+                error();
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean A2() {
+        if (tokens.get(i).equals("id")) {
+            i++;
+            return A3();
+        } else {
+            return false;
         }
     }
 
-    public String reverse(String cad) {
-        // ? Revertir la cadena usando bucle y concatenación
-        String inverted = "";
-        for (int i = cad.length() - 1; i >= 0; i--) {
-            inverted += cad.charAt(i);
+    private boolean A3() {
+        if (tokens.get(i).equals(",")) {
+            i++;
+            if (tokens.get(i).equals("id")) {
+                i++;
+                return true;
+            } else {
+                error();
+            }
+        } else {
+            return true;
         }
-        return inverted;
+        return false;
+    }
+
+    private boolean T() {
+        if (T2()) {
+            return T1();
+        }
+        return false;
+    }
+
+    private boolean T1() {
+        if (tokens.get(i).equals(",")) {
+            i++;
+            if (T()) {
+                return true;
+            } else {
+                error();
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean T2() {
+        if (tokens.get(i).equals("id")) {
+            i++;
+            return T3();
+        } else {
+            return false;
+        }
+    }
+
+    private boolean T3() {
+        if (tokens.get(i).equals("id")) {
+            i++;
+        }
+        return false;
     }
 }
--- src/Tabla.java
+++ src/Tabla.java
@@ -1,133 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tabla {
-    
-    public String[][] tabla = new String[11][8];
 
-    /*
-     *     select D from T  distinct P | P    * | A     A2 A1   , A | Ɛ   id A3   . id | Ɛ    T2 T1 , T | Ɛ
-     *     this.tabla [ x ] [ y ]
-     *                        0        1       2     3   4   5    6    7
-     *                      select   from  distinct  *   ,   .   id  $(EOF)
-     *        0         Q
-     *        1         D
-     *        2         P
-     *        3         A
-     *        4         A1
-     *        5         A2
-     *        6         A3
-     *        7         T
-     *        8         T1
-     *        9         T2
-     *        10        T3
-     */
     public Tabla(){
-       //! Q
-       this.tabla [0] [0] = "select D from T";
-       this.tabla [0] [1] = "!";
-       this.tabla [0] [2] = "!";
-       this.tabla [0] [3] = "!";
-       this.tabla [0] [4] = "!";
-       this.tabla [0] [5] = "!";
-       this.tabla [0] [6] = "!";
-       this.tabla [0] [7] = "!";
 
-       //! D
-       this.tabla [1] [0] = "!";
-       this.tabla [1] [1] = "!";
-       this.tabla [1] [2] = "distinct P";
-       this.tabla [1] [3] = "P";
-       this.tabla [1] [4] = "!";
-       this.tabla [1] [5] = "!";
-       this.tabla [1] [6] = "P";
-       this.tabla [1] [7] = "!";
-
-       //! P
-       this.tabla [2] [0] = "!";
-       this.tabla [2] [1] = "!";
-       this.tabla [2] [2] = "!";
-       this.tabla [2] [3] = "*";
-       this.tabla [2] [4] = "!";
-       this.tabla [2] [5] = "!";
-       this.tabla [2] [6] = "A";
-       this.tabla [2] [7] = "!";
-
-       //! A
-       this.tabla [3] [0] = "!";
-       this.tabla [3] [1] = "!";
-       this.tabla [3] [2] = "!";
-       this.tabla [3] [3] = "!";
-       this.tabla [3] [4] = "!";
-       this.tabla [3] [5] = "!";
-       this.tabla [3] [6] = "A2 A1";
-       this.tabla [3] [7] = "!";
-
-       //! A1
-       this.tabla [4] [0] = "!";
-       this.tabla [4] [1] = "E";
-       this.tabla [4] [2] = "!";
-       this.tabla [4] [3] = "!";
-       this.tabla [4] [4] = ", A";
-       this.tabla [4] [5] = "!";
-       this.tabla [4] [6] = "!";
-       this.tabla [4] [7] = "!";
-
-       //! A2
-       this.tabla [5] [0] = "!";
-       this.tabla [5] [1] = "!";
-       this.tabla [5] [2] = "!";
-       this.tabla [5] [3] = "!";
-       this.tabla [5] [4] = "!";
-       this.tabla [5] [5] = "!";
-       this.tabla [5] [6] = "id A3";
-       this.tabla [5] [7] = "!";
-
-       //! A3
-       this.tabla [6] [0] = "!";
-       this.tabla [6] [1] = "E";
-       this.tabla [6] [2] = "!";
-       this.tabla [6] [3] = "!";
-       this.tabla [6] [4] = "E";
-       this.tabla [6] [5] = ". id";
-       this.tabla [6] [6] = "!";
-       this.tabla [6] [7] = "!";
-
-       //! T
-       this.tabla [7] [0] = "!";
-       this.tabla [7] [1] = "!";
-       this.tabla [7] [2] = "!";
-       this.tabla [7] [3] = "!";
-       this.tabla [7] [4] = "!";
-       this.tabla [7] [5] = "!";
-       this.tabla [7] [6] = "T2 T1";
-       this.tabla [7] [7] = "!";
-
-       //! T1
-       this.tabla [8] [0] = "!";
-       this.tabla [8] [1] = "!";
-       this.tabla [8] [2] = "!";
-       this.tabla [8] [3] = "!";
-       this.tabla [8] [4] = ", T";
-       this.tabla [8] [5] = "!";
-       this.tabla [8] [6] = "!";
-       this.tabla [8] [7] = "E";
-
-       //! T2
-       this.tabla [9] [0] = "!";
-       this.tabla [9] [1] = "!";
-       this.tabla [9] [2] = "!";
-       this.tabla [9] [3] = "!";
-       this.tabla [9] [4] = "!";
-       this.tabla [9] [5] = "!";
-       this.tabla [9] [6] = "id T3";
-       this.tabla [9] [7] = "!";
-
-       //! T3
-       this.tabla [10] [0] = "!";
-       this.tabla [10] [1] = "!";
-       this.tabla [10] [2] = "!";
-       this.tabla [10] [3] = "!";
-       this.tabla [10] [4] = "!";
-       this.tabla [10] [5] = "!";
-       this.tabla [10] [6] = "id";
-       this.tabla [10] [7] = "E";
     }
+
+    public Map < String, Map<String, String> > generarTablaAnalisis() {
+        Map<String, Map<String, String>> tabla = new HashMap<>();
+        //  "Ɛ"   Simbolos de entrada            id|  ,  |sel|dist|from|  *  | .  | $
+        tabla.put("Q", crearFila("!", "!", "1", "!", "!", "!", "!", "!"));
+        tabla.put("D", crearFila("2", "!", "!", "2", "!", "2", "!", "!"));
+        tabla.put("P", crearFila("3", "!", "!", "!", "!", "3", "!", "!"));
+        tabla.put("A", crearFila("4", "!", "!", "!", "!", "!", "!", "!"));
+        tabla.put("A1", crearFila("!", "5", "!", "!", "5", "!", "!", "!"));
+        tabla.put("A2", crearFila("6", "!", "!", "!", "!", "!", "!", "!"));
+        tabla.put("A3", crearFila("!", "7", "!", "!", "7", "!", "7", "!"));
+        tabla.put("T", crearFila("8", "!", "!", "!", "!", "!", "!", "!"));
+        tabla.put("T1", crearFila("!", "9", "!", "!", "!", "!", "!", "9"));
+        tabla.put("T2", crearFila("10", "!", "!", "!", "!", "!", "!", "!"));
+        tabla.put("T3", crearFila("11", "11", "!", "!", "!", "!", "!", "11"));
+
+        return tabla;
+    }
+
+    private Map<String, String> crearFila(String... valores) {
+        Map<String, String> fila = new HashMap<>();
+        fila.put("id", valores[0]);
+        fila.put(",", valores[1]);
+        fila.put("select", valores[2]);
+        fila.put("dinstict", valores[3]);
+        fila.put("from", valores[4]);
+        fila.put("*", valores[5]);
+        fila.put(".", valores[6]);
+        fila.put("$", valores[7]);
+        return fila;
+    }
+
 }
