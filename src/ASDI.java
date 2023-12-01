7e0fc612385f06ed80d932b7aee51686e0a8639a
zVioletta <92064685+zVioletta@users.noreply.github.com>
2023-11-30 18:16:07 -0600

    Reusing past code

--- src/ASDI.java
+++ src/ASDI.java
@@ -1,83 +1,61 @@
 import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
-public class ASDI {
-
-    private int i = 0;
-    private Token aux;
+public class ASDI implements Parser {
+    private int i = 0, j = 0, k = 0;
+    private boolean err = false;
+    private Token preAn;
     private final List<Token> tokens;
-    private final Map<String, String[]> production = new HashMap<>();
-    Tabla t = new Tabla();
-    Map<String, Map<String, String>> table = t.tableAnalysis();
+    private final Tabla ta = new Tabla();
+    private String prod = "", item = "";
+    private final Stack<String> battery = new Stack<>();
+
+    public ASDI(List<Token> tokens) {
+        this.tokens = tokens;
+        preAn = this.tokens.get(i);
+    }
 
     private void error() {
         System.out.println("Error on: " + (i + 1) + ". Expecting:" + tokens.get(i));
     }
 
-    public ASDI(List<Token> tokens) {
-        this.tokens = tokens;
-
-        production.put("Q", new String[] {"select D from T"});
-        production.put("D", new String[] {"distinct P", "P"});
-        production.put("P", new String[] {"*", "A"});
-        production.put("A", new String[] {"A2 A1"});
-        production.put("A1", new String[] {", A", ""});
-        production.put("A2", new String[] {"id A3"});
-        production.put("A3", new String[] {". id", ""});
-        production.put("T", new String[] {"T2 T1"});
-        production.put("T1", new String[] {", T", ""});
-        production.put("T2", new String[] {"id T3"});
-        production.put("T3", new String[] {"id", ""});
+    @Override
+    public boolean parse() {
+        System.out.println("---\nSyntax Analysis\n---");
+        this.battery.push("$");
+        this.battery.push("Q");
+        this.item = battery.pop();
     }
 
-    public boolean analizarEntrada() {
-        Stack<String> battery = new Stack<>();
-        battery.push("$");
-        battery.push("Q");
-        String x = battery.pop();
+    public void addBatteryInv() {
+        battery.pop();
+        char c; // ? Distingue palabras de producciónes
+        String aux = "";
+        int x = this.prod.length() - 1;
 
-        int flag;
-        String[] success;
-
-        while (!(x.equals("$"))) {
-            aux = tokens.get(i);
-            System.out.println("Popped Out:" + x + " Aux:" + aux.getLexema());
-
-            if (x.equals(aux.getLexema())) {
-                i++;
-            }
-            else if (x.isEmpty()) {
-                // TODO: Look if needed
-            }
-            else if (this.isTerminal(x)) {
-                error();
-                return false;
-            }
-            else if ((table.get(x).get(aux.getLexema())).equals("!")) {
-                error();
-                return false;
-            }
-            else if (!table.get(x).get(aux.getLexema()).equals("!")) {
-                success = production.get(x)[0].split(" ");
-                if (production.get(x).length > 1) {
-                    if (success[0].equals(aux.getLexema())) {
-                        flag = 0;
-                    } else {
-                        flag = 1;
-                    }
-                } else {
-                    flag = 0;
+        while (x >= 0) {
+            c = this.prod.charAt(x);// ? Asigna último char del lexema
+            if ((c == ' ') || (x == 0)) {
+                if (x == 0) {
+                    aux += c;
                 }
-                String[] newElement = production.get(x)[flag].split(" ");
-                System.out.println("Output: " + x + "->" + production.get(x)[flag]);
-                for (int j = 0; j < newElement.length; j++) {
-                    battery.push(newElement[newElement.length - j - 1]);
-                }
+                System.out.println("Before reverse: " + aux);
+                this.battery.push(this.reverse(aux));
+                aux = "";
+                System.out.println("Battery start: " + battery.peek());
+            } else {
+                aux += c;
             }
-            x = battery.pop();
+            x--;
         }
-        return true;
+    }
+
+    public String reverse(String cad) {
+        // ? Revertir la cadena usando bucle y concatenación
+        String inverted = "";
+        for (int i = cad.length() - 1; i >= 0; i--) {
+            inverted += cad.charAt(i);
+        }
+        return inverted;
     }
 }
