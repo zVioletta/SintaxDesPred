9ebe20f1629fcb159ef873ce5cac53c283d82b29
zVioletta <92064685+zVioletta@users.noreply.github.com>
2023-11-30 18:20:41 -0600

    Table rebranding! yay i guess

--- src/Tabla.java
+++ src/Tabla.java
@@ -1,114 +1,114 @@
 public class Tabla {
-    public String[][] tabla = new String[11][8];
+    public String[][] t = new String[11][8];
     public Tabla(){
         //! Q
-        this.tabla[0][0] = "select D from T";
-        this.tabla[0][1] = "!";
-        this.tabla[0][2] = "!";
-        this.tabla[0][3] = "!";
-        this.tabla[0][4] = "!";
-        this.tabla[0][5] = "!";
-        this.tabla[0][6] = "!";
-        this.tabla[0][7] = "!";
+        t[0][0] = "select D from T";
+        t[0][1] = "!";
+        t[0][2] = "!";
+        t[0][3] = "!";
+        t[0][4] = "!";
+        t[0][5] = "!";
+        t[0][6] = "!";
+        t[0][7] = "!";
 
         //! D
-        this.tabla[1][0] = "!";
-        this.tabla[1][1] = "!";
-        this.tabla[1][2] = "distinct P";
-        this.tabla[1][3] = "P";
-        this.tabla[1][4] = "!";
-        this.tabla[1][5] = "!";
-        this.tabla[1][6] = "P";
-        this.tabla[1][7] = "!";
+        t[1][0] = "!";
+        t[1][1] = "!";
+        t[1][2] = "distinct P";
+        t[1][3] = "P";
+        t[1][4] = "!";
+        t[1][5] = "!";
+        t[1][6] = "P";
+        t[1][7] = "!";
 
         //! P
-        this.tabla[2][0] = "!";
-        this.tabla[2][1] = "!";
-        this.tabla[2][2] = "!";
-        this.tabla[2][3] = "*";
-        this.tabla[2][4] = "!";
-        this.tabla[2][5] = "!";
-        this.tabla[2][6] = "A";
-        this.tabla[2][7] = "!";
+        t[2][0] = "!";
+        t[2][1] = "!";
+        t[2][2] = "!";
+        t[2][3] = "*";
+        t[2][4] = "!";
+        t[2][5] = "!";
+        t[2][6] = "A";
+        t[2][7] = "!";
 
         //! A
-        this.tabla[3][0] = "!";
-        this.tabla[3][1] = "!";
-        this.tabla[3][2] = "!";
-        this.tabla[3][3] = "!";
-        this.tabla[3][4] = "!";
-        this.tabla[3][5] = "!";
-        this.tabla[3][6] = "A2 A1";
-        this.tabla[3][7] = "!";
+        t[3][0] = "!";
+        t[3][1] = "!";
+        t[3][2] = "!";
+        t[3][3] = "!";
+        t[3][4] = "!";
+        t[3][5] = "!";
+        t[3][6] = "A2 A1";
+        t[3][7] = "!";
 
         //! A1
-        this.tabla[4][0] = "!";
-        this.tabla[4][1] = "E";
-        this.tabla[4][2] = "!";
-        this.tabla[4][3] = "!";
-        this.tabla[4][4] = ", A";
-        this.tabla[4][5] = "!";
-        this.tabla[4][6] = "!";
-        this.tabla[4][7] = "!";
+        t[4][0] = "!";
+        t[4][1] = "E";
+        t[4][2] = "!";
+        t[4][3] = "!";
+        t[4][4] = ", A";
+        t[4][5] = "!";
+        t[4][6] = "!";
+        t[4][7] = "!";
 
         //! A2
-        this.tabla[5][0] = "!";
-        this.tabla[5][1] = "!";
-        this.tabla[5][2] = "!";
-        this.tabla[5][3] = "!";
-        this.tabla[5][4] = "!";
-        this.tabla[5][5] = "!";
-        this.tabla[5][6] = "id A3";
-        this.tabla[5][7] = "!";
+        t[5][0] = "!";
+        t[5][1] = "!";
+        t[5][2] = "!";
+        t[5][3] = "!";
+        t[5][4] = "!";
+        t[5][5] = "!";
+        t[5][6] = "id A3";
+        t[5][7] = "!";
 
         //! A3
-        this.tabla[6][0] = "!";
-        this.tabla[6][1] = "E";
-        this.tabla[6][2] = "!";
-        this.tabla[6][3] = "!";
-        this.tabla[6][4] = "E";
-        this.tabla[6][5] = ". id";
-        this.tabla[6][6] = "!";
-        this.tabla[6][7] = "!";
+        t[6][0] = "!";
+        t[6][1] = "E";
+        t[6][2] = "!";
+        t[6][3] = "!";
+        t[6][4] = "E";
+        t[6][5] = ". id";
+        t[6][6] = "!";
+        t[6][7] = "!";
 
         //! T
-        this.tabla[7][0] = "!";
-        this.tabla[7][1] = "!";
-        this.tabla[7][2] = "!";
-        this.tabla[7][3] = "!";
-        this.tabla[7][4] = "!";
-        this.tabla[7][5] = "!";
-        this.tabla[7][6] = "T2 T1";
-        this.tabla[7][7] = "!";
+        t[7][0] = "!";
+        t[7][1] = "!";
+        t[7][2] = "!";
+        t[7][3] = "!";
+        t[7][4] = "!";
+        t[7][5] = "!";
+        t[7][6] = "T2 T1";
+        t[7][7] = "!";
 
         //! T1
-        this.tabla[8][0] = "!";
-        this.tabla[8][1] = "!";
-        this.tabla[8][2] = "!";
-        this.tabla[8][3] = "!";
-        this.tabla[8][4] = ", T";
-        this.tabla[8][5] = "!";
-        this.tabla[8][6] = "!";
-        this.tabla[8][7] = "E";
+        t[8][0] = "!";
+        t[8][1] = "!";
+        t[8][2] = "!";
+        t[8][3] = "!";
+        t[8][4] = ", T";
+        t[8][5] = "!";
+        t[8][6] = "!";
+        t[8][7] = "E";
 
         //! T2
-        this.tabla[9][0] = "!";
-        this.tabla[9][1] = "!";
-        this.tabla[9][2] = "!";
-        this.tabla[9][3] = "!";
-        this.tabla[9][4] = "!";
-        this.tabla[9][5] = "!";
-        this.tabla[9][6] = "id T3";
-        this.tabla[9][7] = "!";
+        t[9][0] = "!";
+        t[9][1] = "!";
+        t[9][2] = "!";
+        t[9][3] = "!";
+        t[9][4] = "!";
+        t[9][5] = "!";
+        t[9][6] = "id T3";
+        t[9][7] = "!";
 
         //! T3
-        this.tabla[10][0] = "!";
-        this.tabla[10][1] = "!";
-        this.tabla[10][2] = "!";
-        this.tabla[10][3] = "!";
-        this.tabla[10][4] = "!";
-        this.tabla[10][5] = "!";
-        this.tabla[10][6] = "id";
-        this.tabla[10][7] = "E";
+        t[10][0] = "!";
+        t[10][1] = "!";
+        t[10][2] = "!";
+        t[10][3] = "!";
+        t[10][4] = "!";
+        t[10][5] = "!";
+        t[10][6] = "id";
+        t[10][7] = "E";
     }
 }
